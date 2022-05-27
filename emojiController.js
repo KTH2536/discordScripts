5da3735ee83ec5a308e6a9529955a2e42adc6c86
KTH2536 <63619412+KTH2536@users.noreply.github.com>
2022-05-27 15:26:48 +0900

    Update emojiController.js

--- emojiController.js
+++ emojiController.js
@@ -16,3 +16,25 @@ var tagEmoji = (channelId, messageId, emoji) => {
     }, 800);
 }
 // You can get emoji from network tab
+
+// emoji arrays put-delete
+var xhr = new XMLHttpRequest();
+var xhr2 = new XMLHttpRequest();
+var tagEmoji = (channelId, messageId, emoji) => {
+    if (~~(cnt / 5) % 2 == 0) {
+        xhr.open('PUT', '/api/v9/channels/'+channelId+'/messages/'+messageId+'/reactions/'+emoji[cnt%5]+'/@me', true);
+        xhr.setRequestHeader('X-RateLimit-Limit', 5);
+        xhr.setRequestHeader('X-RateLimit-Remaining', 0);
+        xhr.setRequestHeader('authorization', token);
+        xhr.send();
+    }
+    else {
+       xhr2.open('DELETE', '/api/v9/channels/'+channelId+'/messages/'+messageId+'/reactions/'+emoji[emoji.length-1-cnt%5]+'/@me', true);
+       xhr2.setRequestHeader('X-RateLimit-Limit', 5);
+       xhr2.setRequestHeader('X-RateLimit-Remaining', 0);
+       xhr2.setRequestHeader('authorization', token);
+       xhr2.send();
+    }
+    cnt++;
+    setTimeout(() => tagEmoji(channelId, messageId, emoji), 800);
+}
